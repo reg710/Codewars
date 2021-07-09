fecd8d0a1f2c58400536030a38959b7fdde275a8
rcon <reg710@gmail.com>
2021-07-09 17:17:05 -0400

    removed files

--- Ruby/bus.rb
+++
@@ -1,7 +0,0 @@
-data = [0, 1, 2, 3, 4, 5]
-passengers = 0
-0.step(data.length, 2).each do |i|
-    
-end
-
-puts passengers
--- Ruby/fsm.rb
+++ Ruby/fsm.rb
@@ -45,34 +45,38 @@ puts robot.traverse_TCP_states(["RCV_SYN","RCV_ACK","APP_CLOSE"])
 
 
 
-# class Automaton
-#     def initialize
-#       @states = {
-#         "CLOSED" => { "APP_PASSIVE_OPEN" => "LISTEN", "APP_ACTIVE_OPEN" => "SYN_SENT"}, 
-#         "LISTEN" => {"RCV_SYN" => "SYN_RCVD", "APP_SEND" => "SYN_SENT", "APP_CLOSE" => "CLOSED"},
-#         "SYN_RCVD" => {"APP_CLOSE" => "FIN_WAIT_1", "RCV_ACK" => "ESTABLISHED"},
-#         "SYN_SENT" => {"RCV_SYN" => "SYN_RCVD", "RCV_SYN_ACK" => "ESTABLISHED", "APP_CLOSE" => "CLOSED"},
-#         "ESTABLISHED" => {"APP_CLOSE" => "FIN_WAIT_1", "RCV_FIN" => "CLOSE_WAIT"},
-#         "FIN_WAIT_1" => {"RCV_FIN" => "CLOSING", "RCV_FIN_ACK" => "TIME_WAIT", "RCV_ACK" => "FIN_WAIT_2"},
-#         "CLOSING" => {"RCV_ACK" => "TIME_WAIT"},
-#         "FIN_WAIT_2" => {"RCV_FIN" => "TIME_WAIT"},
-#         "TIME_WAIT" => {"APP_TIMEOUT" => "CLOSED"},
-#         "CLOSE_WAIT" => {"APP_CLOSE" => "LAST_ACK"}, 
-#         "LAST_ACK" => {"RCV_ACK" => "CLOSED"}
-#       }
-#     end
+class Machine
+    def initialize
+      @states = {
+        "CLOSED" => { "APP_PASSIVE_OPEN" => "LISTEN", "APP_ACTIVE_OPEN" => "SYN_SENT"}, 
+        "LISTEN" => {"RCV_SYN" => "SYN_RCVD", "APP_SEND" => "SYN_SENT", "APP_CLOSE" => "CLOSED"},
+        "SYN_RCVD" => {"APP_CLOSE" => "FIN_WAIT_1", "RCV_ACK" => "ESTABLISHED"},
+        "SYN_SENT" => {"RCV_SYN" => "SYN_RCVD", "RCV_SYN_ACK" => "ESTABLISHED", "APP_CLOSE" => "CLOSED"},
+        "ESTABLISHED" => {"APP_CLOSE" => "FIN_WAIT_1", "RCV_FIN" => "CLOSE_WAIT"},
+        "FIN_WAIT_1" => {"RCV_FIN" => "CLOSING", "RCV_FIN_ACK" => "TIME_WAIT", "RCV_ACK" => "FIN_WAIT_2"},
+        "CLOSING" => {"RCV_ACK" => "TIME_WAIT"},
+        "FIN_WAIT_2" => {"RCV_FIN" => "TIME_WAIT"},
+        "TIME_WAIT" => {"APP_TIMEOUT" => "CLOSED"},
+        "CLOSE_WAIT" => {"APP_CLOSE" => "LAST_ACK"}, 
+        "LAST_ACK" => {"RCV_ACK" => "CLOSED"}
+      }
+    end
 
-#     def traverse_TCP_states(eventList)
-#         state = "CLOSED"
+    def traverse_TCP_states(eventList)
+        state = "CLOSED"
         
-#         eventList.each do |eventList|
-#             state = @states[state][eventList]
-#         end
+        eventList.each do |eventList|
+            if @states.include? @states[state][eventList]
+                state = @states[state][eventList]
+            else
+                return "ERROR"
+            end
+        end
         
-#         return state
-#     end
-# end
+        return state
+    end
+end
 
-# robot = Automaton.new
+another_robot = Machine.new
 
-# puts robot.traverse_TCP_states(["RCV_SYN","RCV_ACK","APP_CLOSE"])
+puts another_robot.traverse_TCP_states(["RCV_SYN","RCV_ACK","APP_CLOSE"])
