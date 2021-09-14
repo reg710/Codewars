e7565d7aac30f43abe2bbb8011b6c97ed445bf1f
rcon <reg710@gmail.com>
2021-09-14 13:08:19 -0400

    added phone challenge

---
+++ Ruby/phone.rb
@@ -0,0 +1,43 @@
+
+def validPhoneNumber(phoneNumber)
+    chars = phoneNumber.chars
+    return false if chars.length != 14
+    return false if chars[0] != "("
+    return false if chars[4] != ")"
+    return false if chars[5] != " "
+    return false if chars[9] != "-"
+
+    (1..3).each do |num|
+        return false if not_a_digit(chars[num])
+        # if chars[num].ord < 48 || chars[num].ord > 57
+        #     return false
+        # end
+    end
+
+    (6..8).each do |num|
+        return false if not_a_digit(chars[num])
+    end
+
+    (10..13).each do |num|
+        return false if not_a_digit(chars[num])
+    end
+
+    return true
+end
+
+def not_a_digit(string)
+    string.ord < 48 || string.ord > 57
+end
+
+puts not_a_digit("t")
+puts validPhoneNumber("(123) 456-7890")
+
+
+# from codewars
+
+def validPhoneNumber(number)
+    phone = "(000) 000-0000"
+    number.gsub!(/[123456789]/, '0') 
+    return true if phone == number
+    return false
+  end
