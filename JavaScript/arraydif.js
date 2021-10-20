8969162b641bd71cc97aceaee63bf7fbcb150689
rcon <reg710@gmail.com>
2021-10-20 11:29:10 -0400

    Added arraydiff challenge

---
+++ JavaScript/arraydif.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/523f5d21c841566fde000009/train/javascript
+
+
+function arrayDiff(a, b) {
+    let overlap = a.filter(num => !b.includes(num));
+    return overlap;
+}
+
+function arrayDiffLoop(a, b) {
+    let output = [];
+    for (let i = 0; i < a.length; i++){
+        if (!b.includes(a[i])) {
+            output.push(a[i]);
+        }            
+    }
+    return output;
+}
+
+let test = [1,2,2,2,3];
+let test2 = [2];
+console.log(arrayDiffLoop(test, test2));
